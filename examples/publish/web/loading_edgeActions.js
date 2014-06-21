f91baa8f889943f6f9d579385a840b1563574abf
ShogoFujii <sf_1105@hotmail.com>
2014-06-22 00:38:21 +0900

    こっみｔ

---
+++ examples/publish/web/loading_edgeActions.js
@@ -0,0 +1,14 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTimelineAction(compId,symbolName,"Default Timeline","complete",function(sym,e){sym.play();});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'icon'
+(function(symbolName){})("icon");
+//Edge symbol end:'icon'
+})(jQuery,AdobeEdge,"EDGE-45582478");
